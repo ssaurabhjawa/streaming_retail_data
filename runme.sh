dbedcbe67c05818230141425379bb9ab1f2341bc
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-01 19:05:08 +0530

    retail_topic

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
-export SRC_DIR=/home/${USER}/retail_json_data/
+export SRC_DIR=/home/${USER}/streaming_retail_data/retail_topic_data
 export TGT_FOLDER=''
 export TGT_FILE_FORMAT=''
 python3 app.py
